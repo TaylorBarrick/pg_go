89fad8a82a34707f46eeec33de4cb9fe500296d5
Christopher Barrick <Christopher Barrick>
2016-08-24 15:01:08 -0500

    Tests for swarm

--- build.sh
+++ build.sh
@@ -4,7 +4,10 @@
 cd server
 CGO_ENABLED=0 GOOS=linux go build -a -tags netgo -ldflags "-w" -o server
 cd ..
-eval $(docker-machine env --shell=bash default)
+num=$(uname | sed 's/MINGW64_NT-\(.*\)/\1/')
+if [[ "$num" ==  "6.1" ]]; then
+    eval $(docker-machine env --shell=bash)
+fi
 docker-compose down -v
 docker-compose build
 docker-compose up -d
